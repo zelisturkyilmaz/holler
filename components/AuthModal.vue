9e4e2c54f354f0d5e7de01b8f143880db3f80ab8
Zelis Turkyilmaz <zelis.turkyilmaz1@gmail.com>
2024-01-01 17:35:40 +0300

    Modal tabs

---
+++ components/AuthModal.vue
@@ -0,0 +1,63 @@
+<!-- eslint-disable vue/html-self-closing -->
+<script setup>
+const tab = ref('login')
+</script>
+
+<template>
+  <teleport to="body">
+    <div class="fixed inset-0 flex justify-center items-start pt-24 bg-black bg-opacity-30 overflow-y-auto">
+      <div class="bg-white rounded-lg shadow-lg text-left my-8 mx-2 max-w-lg w-full">
+        <div class="p-4">
+          <div class="flex justify-between items-center">
+            <p class="text-2xl font-bold">
+              Your Account
+            </p>
+            <div
+              class="modal-close cursor-pointer"
+            >
+              <Icon name="material-symbols:cancel" size="1.5rem" />
+            </div>
+          </div>
+        </div>
+        <div class="p-4">
+          <ul class="flex mb-4">
+            <li class="flex-auto text-center">
+              <a
+                href="#" class="block rounded p-3 transition"
+                :class="{
+                  'hover:text-white text-white bg-sky-600': tab === 'login',
+                  'hover:text-sky-600': tab === 'register',
+                }"
+                @click.prevent="tab = 'login'"
+              >Login</a>
+            </li>
+            <li class="flex-auto text-center">
+              <a
+                href="#" class="block rounded p-3 transition"
+                :class="{
+                  'hover:text-white text-white bg-sky-600': tab === 'register',
+                  'hover:text-sky-600': tab === 'login',
+                }"
+                @click.prevent="tab = 'register'"
+              >
+                Register</a>
+            </li>
+          </ul>
+
+          <div v-if="tab === 'login'">
+            Login
+          </div>
+          <div v-else>
+            Register
+          </div>
+        </div>
+
+        <div class="flex justify-end items-center p-4">
+          <button class="bg-blue-500 hover:bg-blue-700 px-3 py-1 rounded text-white">
+            Button
+          </button>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     Main
-    <BaseModal />
+    <AuthModal />
   </div>
 </template>
