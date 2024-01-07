7c391ca128be1de776125e6100cb54cfbecba971
Zelis Turkyilmaz <zelis.turkyilmaz1@gmail.com>
2024-01-07 22:03:43 +0300

    Create songlist for homepage

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,15 +4,13 @@ const modal = useModalStore()
 
 <template>
   <header class="bg-gray-700">
-    <nav class="container mx-auto p-4 flex justify-between">
-      <a class="text-white hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out hover:-skew-x-12 font-bold uppercase text-2xl mr-4" href="#">Howler</a>
-      <ul>
-        <li class="text-white">
-          <button class="hover:text-amber-500" @click="modal.showModal()">
-            Login / Register
-          </button>
-        </li>
-      </ul>
+    <nav class="container mx-auto p-4 flex justify-between text-white">
+      <NuxtLink class="hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out hover:-skew-x-12 font-bold uppercase text-2xl mr-4" to="/">
+        Howler
+      </NuxtLink>
+      <button class="hover:text-amber-500" @click="modal.showModal()">
+        Login / Register
+      </button>
     </nav>
   </header>
 </template>
---
+++ pages/song/[id].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    Song ID: {{ route.params.id }}
+  </div>
+</template>
